de528f293cdcf92418e019fde6aa2ce1ecdcce9f
karan kanthan <karankandhan811@gmail.com>
2026-03-05 20:46:40 +0530

    fix api base url for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "https://vitalsync-backend-necr.onrender.com"
+ baseURL: "https://vitalsync-backend-necr.onrender.com/api"
 });
 
 export const getPatients = () => API.get("/patients");
